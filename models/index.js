8f7a6d6b546ed4beeaa1d7d0ff8497ebb86439b7
J. Motyl <webdev@stupiddog.net>
2017-03-28 11:13:05 -0500

    model index.js added

---
+++ models/index.js
@@ -0,0 +1,50 @@
+/* ************************************************************************ */
+/*
+    Database Connection and Model Management - Scalable Version                                  
+
+
+    Mongoose Setup
+
+    Based on our run-time environment choose the appropriate 
+    parameters for connecting to the database
+*/
+var env = process.env.NODE_ENV || 'development';
+var config = require('../mongo-config.json')[env];
+// 
+var mongodbURI = process.env.MONGODB_URI || config.MONGODB_URI;
+
+var mongoose = require('mongoose');
+var Promise = require("bluebird");
+
+// satisfy the Mongoose deprecation warning
+mongoose.Promise = Promise;
+
+var db = {};
+
+// The models... (add/remove as needed)
+db.Fermentable = require('./Fermentable.js');
+db.Hop = require('./Hop.js');
+db.Recipe = require('./Recipe.js');
+//db.User = require('./User.js');
+
+
+// These are nice to have available elsewhere
+db.mongoose = mongoose;
+db.conn = mongoose.connection;
+
+mongoose.connect(mongodbURI, function(err, data){
+	if(err)
+	    console.log(err);
+	else{
+	    console.log("Mongoose connection success");
+	    db.connflag = true;
+	}  
+});
+
+db.conn.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+    throw error;
+});
+
+
+module.exports = db;
