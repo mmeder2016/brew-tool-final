6f614ac38652df56f7a188299c76923e9c76b56e
J. Motyl <webdev@stupiddog.net>
2017-03-30 15:01:12 -0500

    final merge prep

--- webpack.appx.config.js
+++ webpack.appx.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
   // The plain compiled JavaScript will be output into this file
   output: {
-    filename: "public/reactappBundle.js"
+    filename: "public/appXBundle.js"
   },
 
   // This section desribes the transformations we will perform
